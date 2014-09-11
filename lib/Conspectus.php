1364320f999e9cd10978c443404cd9684a1fa53f
Chris Froese <christopherfroese@gmail.com>
2014-09-11 15:07:53 -0600

    New components
    Added new components. Reordered elements in type.

--- lib/Conspectus.php
+++ lib/Conspectus.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Conspectus
+ *
+ * A living style guide written in PHP.
+ * @author Chris Froese <christopherfroese@gmail.com>
+ * @version 1.1
+ */
 
 class Conspectus {
 
@@ -10,42 +17,92 @@ class Conspectus {
 		$json = file_get_contents($config_path);
 		$this->config = json_decode($json, TRUE);
 	}
+
+	/**
+	 * Get style guide title.
+	 * @return string
+	 */
 	function getTitle(){
 		return $this->config['title'];
 	}
 
+	/**
+	* Get style guide author.
+	* @return string
+	*/
 	function getAuthor() {
 		return $this->config['author'];
 	}
 
+	/**
+	* Get style guide author email.
+	* @return string
+	*/
 	function getAuthorEmail() {
 		return $this->config['author_email'];
 	}
 
+	/**
+	* Get style guide author URL.
+	* @return string
+	*/
 	function getAuthorUrl() {
 		return $this->config['author_url'];
 	}
+
+	/**
+	* Get style guide version
+	* @return string
+	*/
 	function getVersion() {
 			return $this->config['version'];
 	}
 
+	/**
+	* Get style revision date
+	* @return string
+	*/
 	function getVersionDate() {
 		return $this->config['version_date'];
 	}
 
+	/**
+	* Get style sheets.
+	* @return array
+	*/
 	function getStyleSheets() {
 		return $this->config['style_sheets'];
 	}
 
+	/**
+	* Get style guide font stacks.
+	*
+	* Retruns an associative array containing $label=>$font_stack pairs.
+	*
+	* @return array
+	*/
 	function getFontStacks() {
 		return $this->config['font_stacks'];
 	}
 
+	/**
+	* Get style guide colors.
+	*
+	* Retruns an associative array containing $label=>$color pairs.
+	*
+	* @return array
+	*/
 	function getColors() {
 		return $this->config['colors'];
 	}
 
-	// Display main navigation from config.
+	/**
+	* Get main navigation from config.
+	*
+	* Retruns an associative array containing $label=>$page pairs.
+	*
+	* @return array
+	*/
 	function getMainNavigation () {
 		$main_nav = array();
 		foreach($this->config['nav'] as $key=>$val) {
@@ -55,10 +112,18 @@ class Conspectus {
 		return $main_nav;
 	}
 
-	// Display title of each markup samples as a select option
-	function getPageNavigation ($type, $strip_numbers = FALSE) {
+	/**
+	* Get page navagation for elements.
+	*
+	* Retruns an associative array containing $anchor=>$title pairs.
+	*
+	* @param string $dir Directory of markup elements to return
+	* @param bool $strip_numbers Weather to remove numbers preceding title.
+	* @return array
+	*/
+	function getPageNavigation ($dir, $strip_numbers = FALSE) {
 		$files = array();
-		$handle=opendir('markup/'.$type);
+		$handle=opendir('markup/'.$dir);
 		while (false !== ($file = readdir($handle))) {
 			if(stristr($file,'.html')){
 				$files[] = $file;
@@ -68,12 +133,15 @@ class Conspectus {
 
 		$page_nav = array();
 		foreach ($files as $file) {
-			$anchor = preg_replace("/\.html$/i", "", $file);
+			$anchor = preg_replace("/\.html$/iu", "", $file);
 			if ($strip_numbers) {
-				$filename = preg_replace("/^[0-9]+_/u", '', $filename);
+				$anchor = preg_replace("/^[0-9]+(\.*[0-9]*)_/u", '', $anchor);
 			}
-			$title = preg_replace(array("/\-/i", "/_/i"), array(" ", ". "), $anchor);
+			$title = preg_replace(array("/\-/", "/_/"), array(" ", ". "), $anchor);
 			$title = ucwords($title);
+
+			$anchor = "con-" . $anchor;
+
 			$page_nav[] = array('anchor'=>$anchor, 'title'=>$title);
 		}
 
@@ -81,10 +149,18 @@ class Conspectus {
 	}
 
 
-	// Display markup view & source
-	function getMarkup($type, $strip_numbers = FALSE) {
+	/**
+	* Get markup for elements.
+	*
+	* Retruns a array containg ('title'=>$title, 'anchor'=>$anchor, 'markup'=>$markup, 'doc'=>$doc) for each element.
+	*
+	* @param string $dir Directory of markup elements to return
+	* @param bool $strip_numbers Weather to remove numbers preceding title.
+	* @return array
+	*/
+	function getMarkup($dir, $strip_numbers = FALSE) {
 		$files = array();
-		$handle=opendir('markup/'.$type);
+		$handle=opendir('markup/'.$dir);
 		while (false !== ($file = readdir($handle))) {
 			if(stristr($file,'.html')) {
 				$files[] = $file;
@@ -98,20 +174,19 @@ class Conspectus {
 			if ($strip_numbers) {
 				$anchor = preg_replace("/^[0-9]+(\.*[0-9]*)_/u", '', $anchor);
 			}
-
 			$title = preg_replace(array("/\-/", "/_/"), array(" ", ". "), $anchor);
 			$title = ucwords($title);
 
 			$anchor = "con-" . $anchor;
 
-			$doc_path = 'doc/'.$type.'/'.$file;
+			$doc_path = 'doc/'.$dir.'/'.$file;
 			if (file_exists($doc_path)) {
 				$doc = file_get_contents($doc_path);
 			} else {
 				$doc = FALSE;
 			}
 
-			$markup = file_get_contents('markup/'.$type.'/'.$file);
+			$markup = file_get_contents('markup/'.$dir.'/'.$file);
 			//echo htmlspecialchars(file_get_contents('markup/'.$type.'/'.$file));
 			$section[] = array('title'=>$title, 'anchor'=>$anchor, 'markup'=>$markup, 'doc'=>$doc);
 		}
